1964ed9089525c8c93d0f718413ffd6d2f057357
vinhn <vinh.nq@nsmv.com.vn>
2023-06-01 16:11:31 +0700

    run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ echo "Start Backup Service"
 #source /opt/database_backup/DatabaseBackup/bin/activate
 #python --version
 touch log.pid
-/opt/database_backup/DatabaseBackup/bin/python3 ./main.py >>./log.pid
+/opt/PythonBackup/bin/python3 ./main.py >>./log.pid
 ps aux | grep main.py
 echo "Start OCR Success"
