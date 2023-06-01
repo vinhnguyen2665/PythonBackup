31a4b0dbf8d514547e7a281e5c0970e1319d7183
Nguyen Quang Vinh <45939115+vinhnguyen2665@users.noreply.github.com>
2023-06-01 16:39:47 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ echo "Start Backup Service"
 #source /opt/database_backup/DatabaseBackup/bin/activate
 #python --version
 touch log.pid
-source env/bin/activate
-python3 ./main.py >>./log.pid
+/opt/PythonBackup/bin/python3 ./main.py >>./log.pid
 ps aux | grep main.py
 echo "Start OCR Success"
